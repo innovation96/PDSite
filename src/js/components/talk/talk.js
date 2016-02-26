da811e4aa099e9aa3d24ecd864e3d9c1213a9402
mssjappjwt <mssjappjwt@gmail.com>
2016-02-26 15:40:06 -0500

    Fix a few things about home

--- src/js/components/talk/talk.js
+++ src/js/components/talk/talk.js
@@ -5,20 +5,33 @@ const Talk = React.createClass({
   // TODO: Line 29 should also have the following markup. But it looks like the info is not a channel.
   // → <Link to={'/channels/' + this.props.talk.channels[0]._id} className="tag">#{this.props.talk.channels[0].name}</Link>
   render() {
+    var link;
+    if (this.props.talk.link) {
+        link = (<div>
+                  <img src="images/link_small_icon.png" width="15" height="15" />
+                  <a href={this.props.talk.link} className="pundit-link">{this.props.talk.link}</a>
+                </div>);
+    }
+    var channel;
+    if (this.props.talk.channels.length) {
+      channel = (<Link to={'/channels/' + this.props.talk.channels[0]._id} className="tag"> → #{this.props.talk.channels[0].name}</Link>);
+    }
     return (
-    <div className="pundit-wrapper">
-      <Link to={'/users/' + this.props.talk.user._id} className="pundit-avatar avatar-small"><img src={this.props.talk.user.profilePicture} width="40" height="40" /></Link>
-      <div className="pundit-details">
-        <div className="pundit-tag">
-          <Link to={'/users/' + this.props.talk.user._id} className="username">{this.props.talk.user.name}</Link>
-        </div>
-        <div className="pundit-subject">
-          <p>{this.props.talk.title}</p>
-          <img src="images/link_small_icon.png" width="15" height="15" />
-          <a href={this.props.talk.shortUrl} className="pundit-link">{this.props.talk.shortUrl}</a>
+      <div className="pundit-wrapper">
+        <Link to={'/users/' + this.props.talk.user._id} className="pundit-avatar avatar-small">
+          <img src={this.props.talk.user.profilePicture} width="40" height="40" />
+        </Link>
+        <div className="pundit-details">
+          <div className="pundit-tag">
+            <Link to={'/users/' + this.props.talk.user._id} className="username">{this.props.talk.user.name}</Link>
+            {channel}
+          </div>
+          <div className="pundit-subject">
+            <p>{this.props.talk.title}</p>
+            {link}
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 });
